443f4fba009399ab0574a2c2d960b76770f248b4
Ole Kröger <o.kroeger@wikunia.de>
2014-03-06 12:55:52 +0100

    prettier indent

--- main.js
+++ main.js
@@ -83,7 +83,7 @@ define(function(require, exports, module) {
                     var summary = tags.s;
                     var syntax = tags.y.replace(/\n/g,'<br>');
                     // indent code if it has space(s) at the beginning of the line
-                    syntax = syntax.replace(/<br>\s(.*?).(.*?)(<br>|$)/g,'<br><p style="text-indent:5px;">$2</p><br>');
+                    syntax = syntax.replace(/<br>\s(.*?).(.*?)(<br>|$)/g,'<br><p style="margin:0 auto; text-indent:5px;">$2</p>');
         
                     // check if function has parameters
                     if (tags.p) { 
@@ -96,6 +96,7 @@ define(function(require, exports, module) {
                     // generate url for read more
                     url = func_class+'/'+func.name;
                     
+                    console.log(syntax);
                     var result = new $.Deferred();
                     var inlineWidget = new InlineDocsViewer(func.name,{SUMMARY:summary, SYNTAX: syntax, URL:url, VALUES:parameters});
                     inlineWidget.load(hostEditor);
